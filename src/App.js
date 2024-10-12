b53ecb3579413d4ed60143a28685f882ab6bc50d
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-10-12 20:45:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
-import HtmlEditor from './HtmlEditor.js';
-import CssEditor from './CssEditor.js';
-import Preview from './Preview.js';
+import HtmlEditor from './HtmlEditor';
+import CssEditor from './CssEditor';
+import Preview from './Preview';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 function App() {
   const [htmlCode, setHtmlCode] = useState('');
   const [cssCode, setCssCode] = useState('');
+  const [activeTab, setActiveTab] = useState('html'); // State to manage active tab
 
   return (
     <div className="h-screen flex flex-col bg-gray-100 text-gray-900">
@@ -23,10 +24,39 @@ function App() {
           </div>
         </div>
       </header>
-      <div className="flex flex-1 overflow-hidden">
-        <HtmlEditor htmlCode={htmlCode} setHtmlCode={setHtmlCode} />
-        <CssEditor cssCode={cssCode} setCssCode={setCssCode} />
+
+      {/* Tab Navigation */}
+      <div className="flex justify-end space-x-2 px-4 pt-1 mb-1"> {/* Reduced margin and padding */}
+        <button
+          onClick={() => setActiveTab('html')}
+          className={`py-2 px-6 rounded-md font-semibold focus:outline-none transition-all ${
+            activeTab === 'html'
+              ? 'bg-indigo-600 text-white hover:bg-indigo-500 shadow-lg'
+              : 'bg-white text-gray-900 hover:bg-gray-100 border border-gray-300'
+          }`}
+        >
+          HTML
+        </button>
+        <button
+          onClick={() => setActiveTab('css')}
+          className={`py-2 px-6 rounded-md font-semibold focus:outline-none transition-all ${
+            activeTab === 'css'
+              ? 'bg-indigo-600 text-white hover:bg-indigo-500 shadow-lg'
+              : 'bg-white text-gray-900 hover:bg-gray-100 border border-gray-300'
+          }`}
+        >
+          CSS
+        </button>
       </div>
+
+      <div className="flex flex-1 overflow-hidden">
+        {activeTab === 'html' ? (
+          <HtmlEditor htmlCode={htmlCode} setHtmlCode={setHtmlCode} />
+        ) : (
+          <CssEditor cssCode={cssCode} setCssCode={setCssCode} />
+        )}
+      </div>
+
       <div className="flex-1 overflow-hidden">
         <Preview htmlCode={htmlCode} cssCode={cssCode} />
       </div>
