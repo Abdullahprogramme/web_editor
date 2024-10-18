9e2745cab4a3c376272fde6a3058c9f31f2efce2
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-10-18 17:55:38 +0500

    Update Preview.js

--- src/Preview.js
+++ src/Preview.js
@@ -10,12 +10,20 @@ function Preview({ htmlCode, cssCode }) {
 
   return (
     <div className="w-full h-full border pt-1 border-gray-300">
-      <iframe
-        className="w-full h-full border-none"
-        srcDoc={srcDoc}
-        title="Preview"
-        sandbox="allow-scripts"
-      />
+      {htmlCode.trim() === '' && cssCode.trim() === '' ? (
+        <div className="w-full h-full flex flex-col items-center justify-center bg-white text-black">
+            <p className="text-lg font-semibold mb-2">Welcome to the code editor</p>
+            <p className="text-md mb-2">Write your code and see the preview here</p>
+            <p className="text-md italic">Happy coding!</p>
+        </div>
+      ) : (
+        <iframe
+            className="w-full h-full border-none"
+            srcDoc={srcDoc}
+            title="Preview"
+            sandbox="allow-scripts"
+        />
+      )}
     </div>
   );
 }
